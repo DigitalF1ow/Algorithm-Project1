d59f273c5cba055b33ce3be68197881b6b9469ad
DigitalF1ow <darrenabbot00@gmail.com>
2020-12-29 12:15:15 +0800

    Randomizer Updated
    Added in a code for randomizer
    Suggestions including regex

--- Randomizer.cpp
+++ Randomizer.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-#include <interator>
+#include <fstream>
+#include <ctime>
 
 using namespace std;
 
@@ -15,16 +16,83 @@ int main()
     cout << "Hello World" << endl;
     cout << "We are going to randomize the emails into each of the following formats:" << endl;
 
-    string alphanum [] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz","1234567890"};
-    string emaildom [] = {"net", "org", "my"};
+    static const char alphanum [] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz" "1234567890"};
+    string emaildom [] = {"net","org","my"};
 
     int sizeA = 100;
     int sizeB = 100000;
     int sizeC = 500000;
 
-    string* setAEmail [] = new string[sizeA];
-    string* setBEmail [] = new string[sizeB];
-    string* setCEmail [] = new string[sizeC];
-
+    string *setAEmail  = new string[sizeA];
+    string *setBEmail  = new string[sizeB];
+    string *setCEmail  = new string[sizeC];
     
+    //Create a loop for the number of letters
+    string generatedEmail;
+    srand(time(NULL));
+    for (int i = 0; i < 5; i++)
+    {
+        generatedEmail += alphanum[rand() %(sizeof(alphanum) - 1)];
+    }
+
+    generatedEmail += ".";
+    
+    for (int i = 0; i < 4; i++)
+    {
+        generatedEmail += alphanum[rand() %(sizeof(alphanum) - 1)];
+    }
+    
+    generatedEmail += "@";
+
+    for (int i = 0; i < 5; i++)
+    {
+        generatedEmail += alphanum[rand() %(sizeof(alphanum) - 1)];
+    }
+
+    generatedEmail += ".";
+    generatedEmail += emaildom[rand() % 3];
+    cout << "Generated Email: " << generatedEmail << endl;
+
+    cout << sizeof(setBEmail);
+    /*
+    //Creating a loop to insert the randomly generated emails into the txt file
+    
+    for (int i = 0; i < (sizeA + sizeB + sizeC); i++)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            generatedEmail += alphanum[rand() %(sizeof(alphanum) - 1)];
+        }
+
+        generatedEmail += ".";
+        
+        for (int i = 0; i < 4; i++)
+        {
+            generatedEmail += alphanum[rand() %(sizeof(alphanum) - 1)];
+        }
+        
+        generatedEmail += "@";
+
+        for (int i = 0; i < 5; i++)
+        {
+            generatedEmail += alphanum[rand() %(sizeof(alphanum) - 1)];
+        }
+
+        generatedEmail += ".";
+        generatedEmail += emaildom[rand() % 3];
+
+        if (sizeof(setAEmail) < sizeA)
+        {
+            setAEmail 
+        }
+        if (sizeof(setBEmail) < sizeB)
+        {
+
+        }
+        if (sizeof(setCEmail) < sizeC)
+        {
+
+        }
+    }
+    */
 }
